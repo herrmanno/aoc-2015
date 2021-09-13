737062bd5c67bf4ad083863490d6731d069fceec
Oliver Herrmann <herrmann@id-on.de>
2021-09-13 07:43:19 +0200

    Add day 4

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Puzzle ( Puzzle, runPuzzle )
 import qualified Day1
 import qualified Day2
 import qualified Day3
+import qualified Day4
 
 main :: IO ()
 main = do
@@ -29,6 +30,7 @@ puzzles =
         [ (Day1.part1, Day1.part2)
         , (Day2.part1, Day2.part2)
         , (Day3.part1, Day3.part2)
+        , (Day4.part1, Day4.part2)
         ]
 
 getPuzzle :: Int -> Maybe (Puzzle, Puzzle)
---
+++ src/Day4.hs
@@ -0,0 +1,25 @@
+{-| Find a string (containing of 'input' + a number 'n') whose MD5 hash will
+a) start with 5 zeros
+b) start with 6 zeros
+-}
+module Day4 where
+
+import Crypto.Hash.MD5 as MD5 ( hash )
+import Data.ByteString.Char8 as BS ( ByteString, take, pack, length )
+import Data.ByteString.Base16 as B16 ( encode )
+import Puzzle (Puzzle)
+
+part1 :: Puzzle
+part1 = show . findHash (BS.pack "00000") . BS.pack
+
+part2 :: Puzzle
+part2 = show . findHash (BS.pack "000000") . BS.pack
+
+findHash :: ByteString -> ByteString -> Int
+findHash match str = go 1 where
+    go n = let h = B16.encode $ MD5.hash (str <> (BS.pack . show) n)
+           in if matches h
+               then n
+               else go (n + 1)
+    matches = (== match) . BS.take (fromIntegral l) -- . show
+    l = BS.length match
