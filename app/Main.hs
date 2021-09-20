edbfda2cfac5e98438ad167ae7f5c7f910f6ab2c
Oliver Herrmann <herrmann@id-on.de>
2021-09-20 10:06:33 +0200

    Turn 'Puzzles' into its own module

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,8 @@
 module Main where
 
 import System.Environment (getArgs)
-import Puzzle ( Puzzle, runPuzzle )
-import qualified Day1
-import qualified Day2
-import qualified Day3
-import qualified Day4
-import qualified Day5
-import qualified Day6
-import qualified Day7
-import qualified Day8
-import qualified Day9
-import qualified Day10
-import qualified Day11
-import qualified Day12
-import qualified Day13
-import qualified Day14
-import qualified Day15
-import qualified Day16
-import qualified Day17
-import qualified Day18
+import Puzzle (runPuzzle)
+import Puzzles (getPuzzle)
 
 main :: IO ()
 main = do
@@ -37,32 +20,3 @@ runDay i select = case getPuzzle i of
         input <- getContents
         mapM_ (`runPuzzle` input) (select p)
     Nothing -> putStrLn $ "Invalid puzzle 'day " <> show i <> "'"
-
--- |All the puzzles for the whole event
-puzzles :: [(Puzzle, Puzzle)]
-puzzles =
-        [ (Day1.part1, Day1.part2)
-        , (Day2.part1, Day2.part2)
-        , (Day3.part1, Day3.part2)
-        , (Day4.part1, Day4.part2)
-        , (Day5.part1, Day5.part2)
-        , (Day6.part1, Day6.part2)
-        , (Day7.part1, Day7.part2)
-        , (Day8.part1, Day8.part2)
-        , (Day9.part1, Day9.part2)
-        , (Day10.part1, Day10.part2)
-        , (Day11.part1, Day11.part2)
-        , (Day12.part1, Day12.part2)
-        , (Day13.part1, Day13.part2)
-        , (Day14.part1, Day14.part2)
-        , (Day15.part1, Day15.part2)
-        , (Day16.part1, Day16.part2)
-        , (Day17.part1, Day17.part2)
-        , (Day18.part1, Day18.part2)
-        ]
-
-getPuzzle :: Int -> Maybe (Puzzle, Puzzle)
-getPuzzle i
-    | i < 1 = Nothing
-    | i > length puzzles = Nothing
-    | otherwise = Just $ puzzles !! (i - 1)
